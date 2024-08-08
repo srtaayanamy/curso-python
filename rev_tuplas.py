d1603cc9ba3a0a90c19eb16575f6769d16924d5f
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-08-08 20:32:02 -0300

    Create rev_tuplas.py

---
+++ rev_tuplas.py
@@ -0,0 +1,16 @@
+#lista imutável
+
+tupla = ('uva', 'uva', 'uva', 'maca', 'banana')
+
+#tupla_uvas = ('uva', 'uva verde', 'uva passa')
+
+lista_frutas = []
+for item in lista:
+    if item == 'uva:':
+        lista_frutas.append(item)
+
+tupla_uvas = {lista_frutas}
+
+#count e index; conta e diz o indice
+
+list(listas)
